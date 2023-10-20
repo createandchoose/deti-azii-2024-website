26b64630d3f0d5caf3396d34b8c7e26b3523b126
Aytal Popov <aytal.popov2002@yandex.ru>
2023-10-20 13:10:47 +0900

    asd

--- sites/2oaJ7CRcDhhQPHKZCcOQwh/chunk-SZSSFQYO.mjs
+++ sites/2oaJ7CRcDhhQPHKZCcOQwh/chunk-SZSSFQYO.mjs
@@ -1,3 +1,2 @@
-function a(t, e) { return { title: "Дети Азии 2024 - Скачать" } }
-export { a };
+function a(t,e){return{title:"App Landing Page - Template"}}export{a};
 //# sourceMappingURL=chunk-SZSSFQYO.mjs.map
