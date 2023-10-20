2ae0c3e1d4f7daf5ba8965cc624cefbcf2c00a93
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2023-10-20 15:40:56 +0900

    asd

--- sites/2oaJ7CRcDhhQPHKZCcOQwh/chunk-SZSSFQYO.mjs
+++ sites/2oaJ7CRcDhhQPHKZCcOQwh/chunk-SZSSFQYO.mjs
@@ -1,2 +1,2 @@
-function a(t,e){return{title:"App Landing Page - Template"}}export{a};
+function a(t,e){return{title:"Скачать - Дети Азии 2024"}}export{a};
 //# sourceMappingURL=chunk-SZSSFQYO.mjs.map
